a8aef6ff5f93fb8bb1376dc744c7166702a809ec
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-12-17 13:20:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,36 @@ def check_first_five(password):
     if  len(password) >= 5 and not password[0:6].isalpha():
         return
     else:
-        print("Password must have number in first 5 characters")
+        print("Password must have number in first 5 characters.")
 def check_last_three(password):
     if  len(password) >= 3 and not password[-3:].isdigit():
         return
     else:
-        print("Password must not have number in last 3 characters")
-
-password = '1eeeeepp1p123'
+        print("Password must not have number in last 3 characters.")
+def check_no_special_characters(password):
+    for character in password:
+        if not (character.isalpha() or character.isdigit()):
+            print('Password cannot contain special characters or symbols!.')
+            break
+    else:
+        return
+def check_for_letter(password):
+    for character in password:
+        if character.isalpha():
+            return
+    else:
+        print('Password must contain at least one letter!')
+def check_for_number (password):
+    for character in password:
+        if character.isdigit():
+            return
+    else:
+        print('Password must contain at least one number!')
+password = 'Abcd2re4'
 check_length(password)
 check_first_five(password)
 check_last_three(password)
+check_for_letter(password)
+check_for_number(password)
+check_no_special_characters(password)
+print('Reading complete')
